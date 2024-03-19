c4013e89122ade97aa951a1c8bfa768ff050cc9e
Nico-Serra <nico.serra000@gmail.com>
2024-03-19 14:45:34 +0100

    add method getUserEmail for generate single Email and add loop for for generate 10 email

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,30 @@
-console.log('hello world');
+//console.log('hello world');
 
 const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            randomListUserEmail:[],
         }
+    },
+    methods: {
+        getUserEmail(){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response)=>{
+                //console.log(response);
+                this.randomListUserEmail.push(response.data.response)
+    
+                //console.log(this.randomListUserEmail);
+            })
+        },
+    },
+    mounted(){
+        //console.log(this.randomListUserEmail);
+       for (let i = 0; i < 10; i++) {
+        const element = this.getUserEmail();
+        
+       };
+       //console.log(this.randomListUserEmail);
     }
 }).mount('#app')
